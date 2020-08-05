c43fae38e6bb598e2f9eae9b2ab4d8cc707be353
IvanScaramouche <ivan.skaram@ya.ru>
2020-08-05 22:58:13 +0700

    Stack

---
+++ Stack_/NodeStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_ {
+    public class NodeStack<T> : IEnumerable<T> {
+        private Node<T> head;
+        private int count;
+
+        public int Count {
+            get {
+                return count;
+            }
+        }
+
+        public bool IsEmpty {
+            get {
+                return count == 0;
+            }
+        }
+
+        public void Push(T item) {
+            var node = new Node<T>(item);
+            node.Next = head;
+            head = node;
+            count++;
+        }
+
+        public T Pop() {
+            if (IsEmpty) {
+                throw new InvalidOperationException("Стек пуст");
+            }
+            Node<T> temp = head;
+            head = head.Next;
+            count--;
+            return temp.Data;
+        }
+
+        public T Peek() {
+            if (IsEmpty) {
+                throw new InvalidOperationException("Стек пуст");
+            }
+            return head.Data;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() {
+            Node<T> current = head;
+
+            while (current != null) {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ Stack_/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Stack_ {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("=> Создаем стек.");
+            var stack = new NodeStack<string>();
+
+            Console.WriteLine("=> Заполняем стек.");
+            stack.Push("Tom");
+            stack.Push("Alice");
+            stack.Push("Bob");
+            stack.Push("Kate");
+
+            foreach (var node in stack) {
+                Console.WriteLine(node);
+            }
+
+            Console.WriteLine();
+
+            var header = stack.Peek();
+            Console.WriteLine($"Верхушка стека: {header}.");
+            Console.WriteLine();
+
+            Console.WriteLine("Убираем верхушку стека.");
+            header = stack.Pop();
+            foreach (var node in stack) {
+                Console.WriteLine(node);
+            }
+
+        }
+    }
+}
